14b867b94ffb837375137365d759801f64df540a
Bohdan Zhuravel <bohdan@zhuravel.bz>
2025-05-27 14:19:25 +0300

    Support enum and virtual columns

--- lib/annotate_rb/model_annotator/column_annotation/annotation_builder.rb
+++ lib/annotate_rb/model_annotator/column_annotation/annotation_builder.rb
@@ -18,6 +18,8 @@ module AnnotateRb
           column_indices = table_indices.select { |ind| ind.columns.include?(@column.name) }
           column_defaults = @model.column_defaults
 
+          @column = enhance_column(@model, @column)
+
           column_attributes = AttributesBuilder.new(@column, @options, is_primary_key, column_indices, column_defaults).build
           formatted_column_type = TypeBuilder.new(@column, @options, column_defaults).build
 
@@ -32,6 +34,45 @@ module AnnotateRb
         end
 
         private
+
+        def enhance_column(model, column)
+          case model.connection.adapter_name
+          when "PostgreSQL"
+            return enhance_postgresql_enum_column(model, column.dup) if column.type == :enum
+          when "Trilogy"
+            return enhance_mysql_virtual_column(model, column.dup) if column.virtual?
+            return enhance_mysql_enum_column(column.dup) if column.sql_type.match?(/\Aenum\b/)
+          end
+
+          column
+        end
+
+        def enhance_mysql_virtual_column(model, column)
+          generation_expression = model.connection.query_value(<<~SQL.squish, "SCHEMA").gsub("\\'", "'").inspect
+            SELECT generation_expression FROM information_schema.columns
+            WHERE table_schema = database()
+              AND table_name = '#{model.table_name}'
+              AND column_name = '#{column.name}'
+          SQL
+
+          column.define_singleton_method(:default_function) { generation_expression }
+          column
+        end
+
+        def enhance_mysql_enum_column(column)
+          enum_values = column.sql_type.scan(/\(([^()]*)\)/)
+
+          column.define_singleton_method(:type) { "enum" }
+          column.define_singleton_method(:limit) { enum_values }
+          column
+        end
+
+        def enhance_postgresql_enum_column(model, column)
+          enum_values = model.connection.select_values("SELECT unnest(enum_range(NULL::#{column.sql_type}))::text")
+
+          column.define_singleton_method(:limit) { enum_values }
+          column
+        end
 
         # TODO: Simplify this conditional
         def is_column_primary_key?(model, column_name)
